5da747f5d25bd59b09438b4f79febe116fdb34e9
FlyByN1ght <savitskiy.daniil@gmail.com>
2023-10-09 15:31:06 +0300

    Uploaded third homework(first branch commit)

---
+++ src/ThirdHW/NewBranch.java
@@ -0,0 +1,8 @@
+package ThirdHW;
+
+public class NewBranch {
+    public static void main(String[] args) {
+        System.out.println("It is first branch for my homework");
+        System.out.println("first commit");
+    }
+}
