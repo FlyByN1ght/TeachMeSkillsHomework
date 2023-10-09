1571ba160727e56dc73567e88f6ceea07e173c25
FlyByN1ght <savitskiy.daniil@gmail.com>
2023-10-09 15:34:48 +0300

    Uploaded third homework(second branch commit)

--- src/ThirdHW/NewBranch.java
+++ src/ThirdHW/NewBranch.java
@@ -3,6 +3,7 @@ package ThirdHW;
 public class NewBranch {
     public static void main(String[] args) {
         System.out.println("It is first branch for my homework");
-        System.out.println("first commit");
+        System.out.println("first commit\n" +
+                "and second commit");
     }
 }
